c931b4f72ad644f385459611805aaf0037255315
Jonathan <jonathanmonteiroa@gmail.com>
2023-08-18 21:28:03 -0300

    primeiro commit

---
+++ src/Users/Moderador.java
@@ -0,0 +1,27 @@
+package Users;
+
+import java.util.Date;
+
+public class Moderador extends Pessoa{
+
+	
+	private String cpf;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Moderador(String nome, String email, String telefone, Date data, String cpf) {
+		super(nome, email, telefone, data);
+		this.cpf = cpf;
+	}
+
+	public boolean cadastrarModerador(String nome, String email, String telefone, Date data, String cpf) {
+		
+	
+	}
+}
---
+++ src/program/MainAGIFF.java
@@ -0,0 +1,9 @@
+package program;
+
+public class MainAGIFF {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
